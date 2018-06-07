3d705b75566a6c1bcf073debb2635e149847913c
Dani <danielnovak0204@gmail.com>
2018-06-07 16:15:28 +0200

    no message

--- KarbantartasNaplo/KarbantartasNaplo/NetworkManager.swift
+++ KarbantartasNaplo/KarbantartasNaplo/NetworkManager.swift
@@ -10,7 +10,20 @@ import UIKit
 import Alamofire
 
 class NetworkManager: NSObject {
-    static func login(completion: @escaping (Data?, Error?) -> Void) {
+    static func login(email: String, password: String, completion: @escaping (Data?, Error?) -> Void) {
+        let headers = [
+            "Content-Type": "application/x-www-form-urlencoded",
+            "hardwareId": UIDevice.current.identifierForVendor!.uuidString,
+            "os": "iOS",
+            "osVersion": UIDevice.current.systemVersion
+        ]
+        let parameters = [
+            "email": email,
+            "password": password
+        ]
         
+        Alamofire.request("http://138.197.187.213/itacademy/login", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseData { response in
+            
+        }
     }
 }
